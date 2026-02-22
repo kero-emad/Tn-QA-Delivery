058d9f06ae3285023082211913bddc7de46241a6
kero-emad <keroemad654@gmail.com>
2026-02-22 09:46:35 +0200

    Remove phone numbers

--- script.js
+++ script.js
@@ -644,10 +644,6 @@ function renderSection(container, sectionInfo, services, isWarning = false) {
             <div class="section-group-header">
                 <h3>${sectionInfo.icon} ${sectionInfo.title}</h3>
                 ${isWarning ? `<div class="warning-box">⚠️ ${sectionInfo.warning}</div>` : ''}
-                <div class="section-phone">
-                    <span>${t('whatsapp_unified')}</span>
-                    <span dir="ltr">${formatPhoneNumber(sectionInfo.phone)}</span>
-                </div>
             </div>
             <div class="services-grid">
     `;
@@ -772,12 +768,6 @@ function createHighlightSectionHTML(service, sectionInfo) {
         <div class="service-section-group highlight-section animate-on-scroll" id="${service.id}-section">
             <div class="section-group-header">
                 <h3>${service.icon} ${text.title}</h3>
-                <div class="phone-display">
-                    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                        <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
-                    </svg>
-                    <span>${formattedPhone}</span>
-                </div>
             </div>
             <div class="highlight-content">
                 <div class="highlight-info">
